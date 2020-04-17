414888fbdff436a4227faf9a2f365902a1d7d9bb
Taner <63161493+taneresen@users.noreply.github.com>
2020-04-17 23:17:06 +0200

    Managed bean(CDI) eklendi

--- src/main/java/com/bilisim/hesapmakinesijsf/JAXRSConfiguration.java
+++ src/main/java/com/bilisim/hesapmakinesijsf/JAXRSConfiguration.java
@@ -3,10 +3,7 @@ package com.bilisim.hesapmakinesijsf;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Configures JAX-RS for the application.
- * @author Juneau
- */
+
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
     
